271a3f6626a92c7acd35c520ffad5b9b1a66001a
Vladislav Dzhidzhoev <dzhidzhoev@gmail.com>
2018-10-10 01:40:46 +0300

    Added necessary functionality.

--- app/src/main/java/com/staplex/tfsht2/MainActivity.java
+++ app/src/main/java/com/staplex/tfsht2/MainActivity.java
@@ -1,10 +1,11 @@
 package com.staplex.tfsht2;
 
-import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.ColorInt;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.HashMap;
@@ -12,48 +13,60 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final HashMap<String, Integer> stations = new HashMap<>();
+    private final HashMap<String, Integer> stations = new HashMap<>();
+    private MetroViewGroup mvgTop, mvgBottom;
 
-    static {
-        stations.put("Водный стадион", android.R.color.holo_green_dark);
-        stations.put("Смоленская", android.R.color.holo_blue_dark);
-        stations.put("Краснопресненская", R.color.brown);
-        stations.put("ВДНХ", android.R.color.holo_orange_dark);
-        stations.put("Планерная", android.R.color.holo_purple);
-        stations.put("Раменки", R.color.yellow);
-        stations.put("Алтуфьево", android.R.color.darker_gray);
-        stations.put("Нагорная", android.R.color.darker_gray);
-        stations.put("Римская", android.R.color.holo_green_light);
-        stations.put("Преображенская площадь", android.R.color.holo_red_dark);
-        stations.put("Новокосино", R.color.yellow);
-        stations.put("Водный стадион 2", android.R.color.holo_green_dark);
-        stations.put("Смоленская 2", android.R.color.holo_blue_dark);
-        stations.put("Краснопресненская 2", R.color.brown);
-        stations.put("ВДНХ 2", android.R.color.holo_orange_dark);
-        stations.put("Планерная 2", android.R.color.holo_purple);
-        stations.put("Раменки 2", R.color.yellow);
-        stations.put("Алтуфьево 2", android.R.color.darker_gray);
-        stations.put("Нагорная 2", android.R.color.darker_gray);
-        stations.put("Римская 2", android.R.color.holo_green_light);
-        stations.put("Преображенская площадь 2", android.R.color.holo_red_dark);
-        stations.put("Новокосино 2", R.color.yellow);
-    }
-
-    private View inflateMetroStationView(ViewGroup root, String name, int color) {
+    private View inflateMetroStationView(ViewGroup root, String name, @ColorInt int color) {
         View v = getLayoutInflater().inflate(R.layout.metro_station_layout, root, false);
         ((TextView) v.findViewById(R.id.metro_station_name)).setText(name);
-        v.findViewById(R.id.location_icon).setBackgroundColor(color);
+        ((ImageView) v.findViewById(R.id.location_icon)).setColorFilter(color);
+        v.setOnClickListener((View view) -> {
+            if (view.getParent() == mvgTop) {
+                mvgTop.removeView(view);
+                mvgBottom.addView(view);
+                mvgBottom.requestLayout();
+            } else {
+                mvgBottom.removeView(view);
+                mvgTop.addView(view);
+                mvgTop.requestLayout();
+            }
+        });
         return v;
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        stations.put("Водный стадион", getResources().getColor(android.R.color.holo_green_dark));
+        stations.put("Смоленская", getResources().getColor(android.R.color.holo_blue_dark));
+        stations.put("Краснопресненская", getResources().getColor(R.color.brown));
+        stations.put("ВДНХ", getResources().getColor(android.R.color.holo_orange_dark));
+        stations.put("Планерная", getResources().getColor(android.R.color.holo_purple));
+        stations.put("Раменки", getResources().getColor(R.color.yellow));
+        stations.put("Алтуфьево", getResources().getColor(android.R.color.darker_gray));
+        stations.put("Нагорная", getResources().getColor(android.R.color.darker_gray));
+        stations.put("Римская", getResources().getColor(android.R.color.holo_green_light));
+        stations.put("Преображенская площадь", getResources().getColor(android.R.color.holo_red_dark));
+        stations.put("Новокосино", getResources().getColor(R.color.yellow));
+        stations.put("Водный стадион 2", getResources().getColor(android.R.color.holo_green_dark));
+        stations.put("Смоленская 2", getResources().getColor(android.R.color.holo_blue_dark));
+        stations.put("Краснопресненская 2", getResources().getColor(R.color.brown));
+        stations.put("ВДНХ 2", getResources().getColor(android.R.color.holo_orange_dark));
+        stations.put("Планерная 2", getResources().getColor(android.R.color.holo_purple));
+        stations.put("Раменки 2", getResources().getColor(R.color.yellow));
+        stations.put("Алтуфьево 2", getResources().getColor(android.R.color.darker_gray));
+        stations.put("Нагорная 2", getResources().getColor(android.R.color.darker_gray));
+        stations.put("Римская 2", getResources().getColor(android.R.color.holo_green_light));
+        stations.put("Преображенская площадь 2", getResources().getColor(android.R.color.holo_red_dark));
+        stations.put("Новокосино 2", getResources().getColor(R.color.yellow));
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        MetroViewGroup top = (MetroViewGroup) findViewById(R.id.mvg_top);
+
+        mvgTop = findViewById(R.id.mvg_top);
+        mvgBottom = findViewById(R.id.mvg_bottom);
         for (Map.Entry<String, Integer> station :
                 stations.entrySet()) {
-            top.addView(inflateMetroStationView(top, station.getKey(), station.getValue()));
+            mvgBottom.addView(inflateMetroStationView(mvgBottom, station.getKey(), station.getValue()));
         }
     }
 }
--- app/src/main/java/com/staplex/tfsht2/MetroViewGroup.java
+++ app/src/main/java/com/staplex/tfsht2/MetroViewGroup.java
@@ -12,15 +12,30 @@ class MetroViewGroup extends ViewGroup {
     }
 
     public MetroViewGroup(Context context, AttributeSet attrs) {
-        this(context, null, 0);
+        this(context, attrs, 0);
     }
 
     public MetroViewGroup(Context context, AttributeSet attrs, int defStyleAttr) {
-        this(context, null, 0, 0);
+        this(context, attrs, defStyleAttr, 0);
     }
 
     public MetroViewGroup(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    protected LayoutParams generateDefaultLayoutParams() {
+        return new MarginLayoutParams(super.generateDefaultLayoutParams());
+    }
+
+    @Override
+    public LayoutParams generateLayoutParams(AttributeSet attrs) {
+        return new MarginLayoutParams(getContext(), attrs);
+    }
+
+    @Override
+    protected LayoutParams generateLayoutParams(LayoutParams p) {
+        return new MarginLayoutParams(p);
     }
 
     @Override
@@ -30,20 +45,20 @@ class MetroViewGroup extends ViewGroup {
         final int lineWidth = parentWidth - getPaddingLeft() - getPaddingRight();
         final int childHeight = MeasureSpec.getSize(heightMeasureSpec) - getPaddingTop() - getPaddingBottom();
 
-        // Measure children
-        measureChildren(MeasureSpec.makeMeasureSpec(lineWidth, MeasureSpec.AT_MOST),
-                MeasureSpec.makeMeasureSpec(childHeight, MeasureSpec.AT_MOST));
-
         // Measure result
         int measuredHeight = getPaddingTop() + getPaddingBottom();
         int currentLineWidth = 0;
         // Max child height in current line
         int maxLineHeight = 0;
+        int measureState = 0;
         for (int i = 0; i < getChildCount(); i++) {
             View child = getChildAt(i);
+            child.measure(MeasureSpec.makeMeasureSpec(lineWidth, MeasureSpec.AT_MOST),
+                    MeasureSpec.makeMeasureSpec(childHeight, MeasureSpec.AT_MOST));
+            measureState = combineMeasuredStates(measureState, child.getMeasuredState());
             MarginLayoutParams layoutParams = (MarginLayoutParams) child.getLayoutParams();
             int deltaWidth = child.getMeasuredWidth() + layoutParams.leftMargin + layoutParams.rightMargin;
-            if (deltaWidth + currentLineWidth > currentLineWidth) {
+            if (deltaWidth + currentLineWidth > lineWidth) {
                 currentLineWidth = 0;
                 measuredHeight += maxLineHeight;
                 maxLineHeight = 0;
@@ -52,8 +67,9 @@ class MetroViewGroup extends ViewGroup {
             maxLineHeight = Math.max(maxLineHeight, child.getMeasuredHeight() + layoutParams.topMargin + layoutParams.bottomMargin);
         }
 
-        setMeasuredDimension(parentWidth,
-                (measuredHeight > parentHeight ? MEASURED_STATE_TOO_SMALL : 0) | measuredHeight);
+        setMeasuredDimension(resolveSizeAndState(parentWidth, widthMeasureSpec, measureState),
+                resolveSizeAndState((measuredHeight > parentHeight ? MEASURED_STATE_TOO_SMALL : 0) | measuredHeight,
+                        heightMeasureSpec, measureState));
     }
 
     @Override
@@ -67,16 +83,21 @@ class MetroViewGroup extends ViewGroup {
             View child = getChildAt(i);
             MarginLayoutParams layoutParams = (MarginLayoutParams) child.getLayoutParams();
             int deltaWidth = child.getMeasuredWidth() + layoutParams.leftMargin + layoutParams.rightMargin;
-            if (deltaWidth + currentLineWidth > currentLineWidth) {
+            if (deltaWidth + currentLineWidth > lineWidth) {
                 currentLineWidth = 0;
                 currentHeight += maxLineHeight;
                 maxLineHeight = 0;
             }
             currentLineWidth += deltaWidth;
-            int x = left + lineWidth - deltaWidth + layoutParams.leftMargin;
-            int y = top + currentHeight + layoutParams.topMargin;
-            child.layout(x, y, x + child.getMeasuredWidth(), y + getMeasuredHeight());
+            int x = getPaddingLeft() + lineWidth - currentLineWidth + layoutParams.leftMargin;
+            int y = currentHeight + layoutParams.topMargin;
+            child.layout(x, y, x + child.getMeasuredWidth(), y + child.getMeasuredHeight());
             maxLineHeight = Math.max(maxLineHeight, child.getMeasuredHeight() + layoutParams.topMargin + layoutParams.bottomMargin);
         }
+    }
+
+    @Override
+    protected boolean checkLayoutParams(LayoutParams p) {
+        return p instanceof MarginLayoutParams;
     }
 }
