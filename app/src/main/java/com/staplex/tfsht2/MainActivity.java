51f32d94d6c3be53de71cd1122f84dcc5512cad2
Vladislav Dzhidzhoev <dzhidzhoev@gmail.com>
2018-10-11 03:22:05 +0300

    ADDITION: added comments.

--- app/src/main/java/com/staplex/tfsht2/MainActivity.java
+++ app/src/main/java/com/staplex/tfsht2/MainActivity.java
@@ -21,15 +21,21 @@ import java.util.Map;
 public class MainActivity extends AppCompatActivity {
 
     private final HashMap<String, Integer> stations = new HashMap<>();
+    // Top and bottom panels
     private MetroViewGroup mvgTop, mvgBottom;
 
+    // Inflate view for station
     private View inflateMetroStationView(ViewGroup root, String name, @ColorInt int color) {
         View v = getLayoutInflater().inflate(R.layout.metro_station_layout, root, false);
+
+        // bind content
         ((TextView) v.findViewById(R.id.metro_station_name)).setText(name);
         ((ImageView) v.findViewById(R.id.location_icon)).setColorFilter(color);
-        v.setTransitionName(name);
+
+        // add click listener
         v.setOnClickListener((View view) -> {
             MetroViewGroup oldParent, newParent;
+            // decide where we are moving from and to
             if (view.getParent() == mvgTop) {
                 oldParent = mvgTop;
                 newParent = mvgBottom;
@@ -38,27 +44,36 @@ public class MainActivity extends AppCompatActivity {
                 newParent = mvgTop;
             }
 
+            // inflate new view with the same parameters, but initially invisible
+            // (to determine its layout)
             View newView = inflateMetroStationView(newParent, name, color);
             newView.setVisibility(View.INVISIBLE);
+
+            // we need to start animation after we determine the layout of the view in a new container
             newView.addOnLayoutChangeListener((nView, left, top, right, bottom, oldLeft, oldTop, oldRight, oldBottom) -> {
                 if (newView.getParent() != null) {
+                    // calculate position of new view in old parent's coordinate system
                     Rect to = new Rect(left, top, right, bottom);
                     to.offset(newParent.getLeft(), newParent.getTop());
                     to.offset(-oldParent.getLeft(), -oldParent.getTop());
+
                     Path path = new Path();
                     path.setLastPoint(view.getX(), view.getY());
                     path.lineTo(to.left, to.top);
+
+                    // setup animation
                     ObjectAnimator animator = ObjectAnimator.ofFloat(view, "x", "y", path)
                             .setDuration(500);
                     animator.setInterpolator(new AccelerateDecelerateInterpolator());
                     animator.addListener(new Animator.AnimatorListener() {
                         @Override
                         public void onAnimationStart(Animator animation) {
-
                         }
 
                         @Override
                         public void onAnimationEnd(Animator animation) {
+                            // when animantion ends removes old view and makes the new one visible
+                            // (with layout bounds change animation)
                             TransitionManager.beginDelayedTransition(root, new ChangeBounds());
                             oldParent.removeView(view);
                             newView.setVisibility(View.VISIBLE);
@@ -66,17 +81,16 @@ public class MainActivity extends AppCompatActivity {
 
                         @Override
                         public void onAnimationCancel(Animator animation) {
-
                         }
 
                         @Override
                         public void onAnimationRepeat(Animator animation) {
-
                         }
                     });
                     animator.start();
                 }
             });
+
             newParent.addView(newView);
         });
         return v;
@@ -95,6 +109,7 @@ public class MainActivity extends AppCompatActivity {
         stations.put("Римская", getResources().getColor(android.R.color.holo_green_light));
         stations.put("Преображенская площадь", getResources().getColor(android.R.color.holo_red_dark));
         stations.put("Новокосино", getResources().getColor(R.color.yellow));
+        // let's add some fiction stations to save the time
         stations.put("Водный стадион 2", getResources().getColor(android.R.color.holo_green_dark));
         stations.put("Смоленская 2", getResources().getColor(android.R.color.holo_blue_dark));
         stations.put("Краснопресненская 2", getResources().getColor(R.color.brown));
@@ -112,6 +127,8 @@ public class MainActivity extends AppCompatActivity {
 
         mvgTop = findViewById(R.id.mvg_top);
         mvgBottom = findViewById(R.id.mvg_bottom);
+
+        // view stations
         for (Map.Entry<String, Integer> station :
                 stations.entrySet()) {
             mvgBottom.addView(inflateMetroStationView(mvgBottom, station.getKey(), station.getValue()));
